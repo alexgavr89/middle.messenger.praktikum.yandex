8a900dce17a6f91c9d56d05c10b4c9a4c8603c55
alexgavr89 <55540013+alexgavr89@users.noreply.github.com>
2021-03-28 19:33:42 +0500

    Изменил server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/deploy'));
 app.get('/', (req, res) => {
     res
         .status(200)
-        .sendFile(__dirname + '/deploy/login.html')
+        .sendFile(__dirname + '/deploy/index.html')
 });
 
 app.listen(PORT, function () {

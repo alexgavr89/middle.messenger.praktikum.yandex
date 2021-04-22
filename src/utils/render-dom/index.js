599a6acb13fff7f071b262b19ac5b5799394da0e
Gavrilkov Alexandr <alexgavr89@gmail.com>
2021-04-22 10:11:42 +0500

    Sprin_2

--- src/utils/render-dom/index.js
+++
@@ -1,6 +0,0 @@
-export function render(query, block) {
-    const root = document.querySelector(query);
-
-    root.insertBefore(block.getContent(), document.querySelector('script'));
-    return root;
-}
